020c917c336a7cb26b90b906616dbc490f2dba6f
ch-arslanahmad <ch.arslanad@gmail.com>
2026-07-21 17:59:54 +0500

    feat(job-detail): implement job application dialog and enhance job details display

--- frontend/src/pages/JobDetail.tsx
+++ frontend/src/pages/JobDetail.tsx
@@ -1,67 +1,88 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
 
 import type { Job } from "../types";
 
 function JobDetail() {
-  const [isApplied, setIsApplied] = useState(false);
-  const [job, setJob] = useState<Job | null>(null);
-  const [error, setError] = useState("");
+  const job: Job = {
+      id: 0,
+      recruiter_id: 10,
+      title: "Job Title",
+      about_role: "Job Description",
+      requirements: "know this, know that, professional in speech",
+      responsibilities: "responsibility 1, responsibility 2, responsibility 3",
+      location: "lahore",
+      salary: 1000,
+      type: "full-time",
+      company: "Google",
+      created_at: "2026-01-15"
+  };
+  const dialogRef = useRef<HTMLDialogElement>(null);
+  const [applied, setApplied] = useState(false);
 
-  const { id } = useParams();
 
-  useEffect(() => {
-    fetch(`/api/jobs/${id}`)
-      .then((response) => {
-        if (!response.ok) throw new Error("Server error");
-        return response.json();
-      })
-      .then((data) => setJob(data.job))
-      .catch((err) => {
-        if (err.message === "Failed to fetch")
-          setError("Backend is not running");
-        else setError(err.message);
-      });
-  }, [id]);
-
-  if (error) return <p style={{ color: "red" }}>{error}</p>;
-  if (!job) return <p>Not Found...</p>;
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log("Form submitted!");
+    setApplied(true);
+    dialogRef.current?.close();
+  }
 
   return (<>
     <div className="job-details">
-      <div id="main-content">
-        <h2 id="title">{job.title}</h2>
-        <p id="about-role">{job.about_role}</p>
+      <div className="main-content">
+        <h2>{job.title}</h2>
+        <p className="about-role">{job.about_role}</p>
         <h3>Requirements</h3>
-        <div id="requirements">{job.requirements?.split(",").map((skill, i) => <span key={i}>{skill}</span>)}</div>
+        <div className="requirements">{job.requirements?.split(",").map((skill, i) => <span key={i} className="skill-tag">{skill}</span>)}</div>
         <h3>Responsibilities</h3>
-        <div id="responsibilities">{job.responsibilities?.split(",").map((responsibility, i) => <span key={i}>{responsibility}</span>)}</div>
+        <div className="responsibilities">{job.responsibilities?.split(",").map((responsibility, i) => <span key={i} className="skill-tag">{responsibility}</span>)}</div>
       </div>
-      <div id="side-bar">
-        <div id="company">
-          <i className="fas fa-building"></i>
+      <div className="side-bar">
+        <div className="sidebar-item">
+          <i className="fas fa-building sidebar-icon"></i>
           {job.company}
         </div>
-        <div id="location">
-          <i className="fas fa-location"></i>
+        <div className="sidebar-item">
+          <i className="fas fa-location sidebar-icon"></i>
           {job.location}
         </div>
-        <div id="type">
-          <i className="fas fa-briefcase"></i>
+        <div className="sidebar-item">
+          <i className="fas fa-briefcase sidebar-icon"></i>
           {job.type}
         </div>
-        <div id="salary">
-          <i className="fas fa-money-bill-wave"></i>
+        <div className="sidebar-item">
+          <i className="fas fa-money-bill-wave sidebar-icon"></i>
           ${job.salary.toLocaleString()}
         </div>
-        {job.created_at && <div id="created-at">
-          <i className="fas fa-calendar"></i>
+        {job.created_at && <div className="sidebar-item">
+          <i className="fas fa-calendar sidebar-icon"></i>
         {new Date(job.created_at).toLocaleDateString('en-US', { month: 'long', year: 'numeric' })}
       </div>}
-      <button onClick={() => setIsApplied(true)} disabled={isApplied}>
-        {isApplied ? "Already Applied" : "Apply"}
-        </button>
+      <button onClick={() => dialogRef.current?.showModal()} disabled={applied}>
+        {applied ? 'Applied' : 'Apply'}
+      </button>
+      <dialog ref={dialogRef} onClick={(e) => { if (e.target === dialogRef.current) dialogRef.current?.close(); }}>
+          <div className="dialog-header">
+            <div className="form-job-detail">
+              <h2>Apply to {job.title}</h2>
+              <p>{job.company}</p>
+            </div>
+            <button className="dialog-close" onClick={() => dialogRef.current?.close()}>
+              <i className="fa-solid fa-xmark"></i>
+            </button>
         </div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="coverLetter">Cover Letter</label>
+            <input type="file" id="coverLetter" name="coverLetter" accept=".pdf" />
+            <label htmlFor="resume">Resume</label>
+         <input type="file" id="resume" name="resume" accept=".pdf" />
+          <div className="form-actions">
+            <button type="button" className="btn-cancel" onClick={() => dialogRef.current?.close()}>Cancel</button>
+            <button type="submit" >Submit Application</button>
+          </div>
+        </form>
+      </dialog>
+      </div>
     </div>
   </>
   );
